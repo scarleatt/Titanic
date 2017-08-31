d3082ca1d6dca5db37f7d9d26b97e1531606f71c
resccu <fawnliu2333@gmail.com>
2017-08-31 17:05:06 +0800

    add: merge family

--- titanic.py
+++ titanic.py
@@ -17,6 +17,8 @@ def loadDataSet(filename):
         }.get(lineArr[-1], -1)
         labelMat.append(int(lineArr[1]))
         del(lineArr[0]); del(lineArr[1])
+        lineArr[3] = int(lineArr[3])+int(lineArr[4])
+        del(lineArr[4])
         dataMat.append(lineArr)
     m,n = shape(dataMat)
     sum = 0
@@ -29,6 +31,15 @@ def loadDataSet(filename):
         if dataMat[i][2] == 0:
             dataMat[i][2] = sum
         dataMat[i] = map(float, dataMat[i])
+    return dataMat, labelMat
+
+def loadDataSet2(filename):
+    dataMat = []; labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
     return dataMat, labelMat
 
 def dataNormal(dataMat):
@@ -46,7 +57,12 @@ def dataNormal(dataMat):
 def sigmod(inx):
     return 1.0/(1+exp(-inx))
 
-def gradAscent(dataMat, classLabels):
+def classifyVector(inX, weights):
+    prob = sigmod(sum(inX*weights))
+    if prob > 0.5: return 1
+    else: return 0
+
+def grandAscent(dataMat, classLabels):
     m, n = shape(dataMat)
     alpha = 0.001
     weights = ones(n)
@@ -56,28 +72,77 @@ def gradAscent(dataMat, classLabels):
         weights = weights + alpha * error * dataMat[k]
     return weights
 
-def loadDataSet2(filename):
-    dataMat = []; labelMat = []
-    fr = open(filename)
-    for line in fr.readlines():
-        lineArr = line.strip().split()
-        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
-        labelMat.append(int(lineArr[2]))
-    return dataMat, labelMat
+def grandAscent1(dataMat, classLabels, numIter=150):
+    m, n = shape(dataMat)
+    weights = ones(n)
+    for i in range(numIter):
+        dataIndex = range(m)
+        for j in range(m):
+            alpha = 4/(i+j+1.0) + 0.01
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmod(sum(dataMat[randIndex]*weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMat[randIndex]
+            del(dataIndex[randIndex])
+    return weights
 
-def classifyVector(inX, weights):
-    prob = sigmod(sum(inX*weights))
-    if prob > 0.5: return 1
-    else: return 0
+def selectRandJ(i, m):
+    j = i
+    while j == i:
+        j = int(random.uniform(0, m))
+    return j
+
+def smo(dataMatIn, labelMatIn, C, tol, maxIter):
+    dataMat = mat(dataMatIn); labelMat = mat(labelMatIn).transpose()
+    b = 0; passes = 0
+    m, n = shape(dataMat)
+    alpha = mat(zeros((m,1)))
+    while (passes < maxIter):
+        alphaChanged = 0
+        for i in range(m):
+            fxi = float(multiply(alpha,labelMat).T*(dataMat*dataMat[i,:].T)) + b
+            Ei = fxi - float(labelMat[i])
+            if ((labelMat[i]*Ei < -tol) & (alpha[i] < C)) | ((labelMat[i]*Ei > tol) & (alpha[i] > 0)):
+                j = selectRandJ(i, m)
+                fxj = float(multiply(alpha,labelMat).T*(dataMat*dataMat[j,:].T)) + b
+                Ej = fxj - labelMat[j]
+                alphaIold = alpha[i].copy(); alphaJold = alpha[j].copy()
+                if labelMat[i] != labelMat[j]:
+                    L = max(0, alpha[j]-alpha[i])
+                    H = min(C, C+alpha[j]-alpha[i])
+                else:
+                    L = max(0, alpha[i]+alpha[j]-C)
+                    H = min(C, alpha[i]+alpha[j])
+                if L == H: continue
+                eta = 2.0*dataMat[i,:]*dataMat[j,:].T - dataMat[i,:]*dataMat[i,:].T - dataMat[j,:]*dataMat[j,:].T
+                if eta >= 0: continue
+                alpha[j] = alpha[j] - labelMat[j]*(Ei-Ej)/eta
+
+                if alpha[j] > H: alpha[j] = H
+                elif alpha[j] < L: alpha[j] = L
+
+                if abs(alpha[i]-alpha[j]) < 0.00001: continue
+                alpha[i] = alpha[i] + labelMat[i]*labelMat[j]*(alphaJold-alpha[j])
+                b1 = b-Ei-labelMat[i]*(alpha[i]-alphaIold)*dataMat[i,:]*dataMat[i,:].T - labelMat[j]*(alpha[j]-alphaJold)*dataMat[i,:]*dataMat[j,:].T
+                b2 = b-Ej-labelMat[i]*(alpha[i]-alphaIold)*dataMat[i,:]*dataMat[j,:].T - labelMat[j]*(alpha[j]-alphaJold)*dataMat[j,:]*dataMat[j,:].T
+
+                if (alpha[i] > 0) & (alpha[i] < C): b = b1
+                elif (alpha[j] > 0) & (alpha[j] < C): b = b2
+                else: b = (b1+b2)/2
+                alphaChanged += 1
+        if alphaChanged == 0:
+            passes += 1
+        else:
+            passes = 0
+    return b, alpha
 
 def colicTest():
     trainMat, trainLabels = loadDataSet('./data/train.csv')
-    weights = gradAscent(array(trainMat), trainLabels)
+    weights = grandAscent(array(trainMat), trainLabels)
     dataMat = []
     fr = open('./data/test.csv')
-    m, n = shape(trainMat)
-    result = [];  sequences = []
-    s = ""
+    sequences = []
+    s = "PassengerId,Survived\n"
     for line in fr.readlines():
         lineArr = line.strip().split(',')
         del(lineArr[2]); del(lineArr[2]); del(lineArr[6]); del(lineArr[-2])
@@ -109,10 +174,16 @@ def colicTest():
             dataMat[i][6] = sumTmp
         dataMat[i] = map(float, dataMat[i])
         del(dataMat[i][0])
+        dataMat[i][4] += dataMat[i][5]; del(dataMat[i][5])
         s += str(sequences[i])+","+str(classifyVector(array(dataMat[i]), weights))+"\n"
-        result.append([sequences[i], classifyVector(array(dataMat[i]), weights)])
     file=open('result.txt','w')
     file.write(s)
     file.close()
 
 colicTest()
+# trainMat, trainLabels = loadDataSet('./data/train.csv')
+
+# print mat(trainMat)
+# b, alphas = smoSimple(trainMat, trainLabels, 0.6, 0.001, 40)
+# print b
+# print alphas[alphas>0]
